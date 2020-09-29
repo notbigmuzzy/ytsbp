16eef5fd6a92b5ea142e7070788830ff271f0899
nbm <notbigmuzzy@gmail.com>
2020-09-29 18:42:15 +0200

    Filter logic and separate JS

---
+++ themes/logic.js
@@ -0,0 +1,54 @@
+//INIT LAZY LOAD
+const observer = lozad();
+observer.observe();
+
+//IFRAME POPUP
+let iframePopup = document.getElementById('iframe-popup');
+let iframeClose = document.getElementById('iframe-close');
+document.addEventListener('click', function (event) {
+    if (!event.target.matches('.iframe-source')) {
+        return
+    }
+    event.preventDefault();
+    let iframeLink = event
+        .target
+        .getAttribute('href');
+    iframePopup
+        .classList
+        .add('show');
+    iframePopup.src = iframeLink
+}, false);
+document.addEventListener('click', function (event) {
+    if (!event.target.matches('#iframe-close')) {
+        return
+    }
+    event.preventDefault();
+    iframePopup
+        .classList
+        .remove('show');
+    iframePopup.src = ''
+}, false);
+document.addEventListener('keydown', event => {
+    if (event.isComposing || event.keyCode === 27) {
+        iframePopup
+            .classList
+            .remove('show');
+        iframePopup.src = ''
+    }
+});
+
+//FILTER
+document.addEventListener('DOMContentLoaded', function () {
+    document
+        .getElementById('search-input')
+        .addEventListener('keyup', function () {
+            let filterRegex = new RegExp('\\b' + this.value, 'gi');
+            document
+                .querySelectorAll('section')
+                .forEach(function (item) {
+                    item
+                        .classList
+                        .toggle('hide', !item.innerText.match(filterRegex))
+                })
+        })
+});
